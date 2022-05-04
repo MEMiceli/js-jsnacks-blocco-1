2c50c65edb1b455bbfb162caa2aca017a8921aef
MEMiceli <marcello.emilio.miceli@gmail.com>
2022-05-04 12:29:45 +0200

    dopo correzione

---
+++ snack-3/main.js
@@ -0,0 +1,25 @@
+// Il software deve chiedere per 10 volte all’utente di inserire un numero. Il programma stampa la somma di tutti i numeri inseriti.
+
+// chiedere numero
+
+// dopo correzione
+let total = 0
+
+for (let i = 0; i < 10; i++){
+    let numero = Number(prompt("inserisci numero"));
+
+    total += numero;
+}
+console.log("number")
+
+// ripeterlo per 10 volte 
+
+// inserire numeri in una lista
+
+// numeri = ['numero', 'numero','numero', 'numero', 'numero','numero', 'numero','numero', 'numero', 'numero']
+
+// for (let i = 0; i < numero[i]; i++){
+
+// }
+
+// sommare i 10 numeri
