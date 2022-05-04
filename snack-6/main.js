e4ed7b51399b3878d0c0cb5579b9661a61c1747e
MEMiceli <marcello.emilio.miceli@gmail.com>
2022-05-04 16:22:41 +0200

    logica fatta

---
+++ snack-6/main.js
@@ -0,0 +1,9 @@
+// Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
+
+// inserisci numero
+
+// salva numero
+
+// se numero / 2 = 0 stampa
+
+// altrimenti numero + 1 e stampa
